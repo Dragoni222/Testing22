9a9300757fab6c466a90b22f0d4ba11e64447161
Dragoni222 <32797625+Dragoni222@users.noreply.github.com>
2020-10-21 11:36:43 -0500

    Initial check-in of module Testing22

---
+++ Testing22/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace Testing22
+{
+    class MainClass
+    {
+        public static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
